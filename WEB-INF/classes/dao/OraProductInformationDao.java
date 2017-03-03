06b191b8da02d745cc7b6e8a3d208fbd833ca6e6
HikaruUtsuno <hu15304004@ga.tera-house.ac.jp>
2017-03-03 15:24:35 +0900

    ResultSetのgetStringを消して、getIntに変更

--- WEB-INF/classes/dao/OraProductInformationDao.java
+++ WEB-INF/classes/dao/OraProductInformationDao.java
@@ -1,14 +1,12 @@
 //Product表のデータ取得
 package dao;
 
-import java.sql.ResultSet;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import bean.ProductInformationBean;
 import ex.ConnectorException;
@@ -18,66 +16,59 @@ import ex.IntegrationException;
 public class OraProductInformationDao implements ProductInformationDao {
 	/*全ての商品の情報を取得するメソッド*/
 	public List getProductInformations() throws IntegrationException{
-		
+
 		ArrayList<ProductInformationBean> infoList =
 							new ArrayList<ProductInformationBean>();
 		Connection connection = null;
 		Statement statement = null;
 		ResultSet result = null;
-		
+
 		try{
 			/*データベースへの接続*/
-			connection 
+			connection
 			= new OracleConnector("shop_admin","admin").getConnection();
 		}catch(IntegrationException e){
 			throw new ConnectorException(e.getMessage(), e);
 		}
-		
+
 		try{
 			/*オートコミット機能をOFFにする*/
 			connection.setAutoCommit(false);
-			
+
 			/*SQL文をDBに送るためのStatementオブジェクトを生成*/
 			statement = connection.createStatement();
-			
+
 			/*SQL文を記述*/
 			String sql = "select product_id, product_name, product_price, "+
 							"product_description, category_id,"+
 							"sub_category_id, product_size, product_color,"+
        						"product_release_date, product_stock_count "+
-							"FROM product_information_view " + 
+							"FROM product_information_view " +
 							"ORDER BY product_release_date DESC";
-			
+
 			/*SQL文を実行し、ResultSetオブジェクトを生成*/
 			result = statement.executeQuery(sql);
-			
+
 			while(result.next()){
 				/*SELECTの結果をBeanに入れる*/
 				ProductInformationBean info = new ProductInformationBean();
-				
+
 				info.setProductId(result.getString(1));
 				info.setProductName(result.getString(2));
-				info.setProductPrice(Integer.parseInt(result.getString(3)));
+				info.setProductPrice(result.getInt(3));
 				info.setProductDescription(result.getString(4));
-				info.setCategoryId(Integer.parseInt(result.getString(5)));
-				info.setSubCategoryId(Integer.parseInt(result.getString(6)));
+				info.setCategoryId(result.getInt(5));
+				info.setSubCategoryId(result.getInt(6));
 				info.setProductSize(result.getString(7));
 				info.setProductColor(result.getString(8));
 				info.setProductReleaseDate(result.getString(9));
-				/*product_stock表の内容が作成されていない場合、
-				  result.getString(10)でnullが返ってくる。
-				  この場合、例外を防ぐため0を返すことにする*/
-				if(result.getString(10) != null){
-					info.setProductStockCount(
-						Integer.parseInt(result.getString(10)));
-				}else{
-					info.setProductStockCount(0);
-				}
+				info.setProductStockCount(result.getInt(10));
+
 				/*ListにBeanを入れる*/
 				infoList.add(info);
 			}
-			
-			
+
+
 		}catch(SQLException e){
 			throw new IllegalSQLException(e.getMessage(), e);
 		}finally{
@@ -100,9 +91,9 @@ public class OraProductInformationDao implements ProductInformationDao {
 				}
 			}
 		}
-			
+
 		//Product表全件を返す。
 		return infoList;
-		
+
 	}
 }
