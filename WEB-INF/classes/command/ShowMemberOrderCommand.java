24bbd662541841154c086c527f189e9337981de9
harukikubota <hk15304009@ga.tera-house.ac.jp>
2017-03-04 10:56:51 +0900

    member_idをsessionScopeから取得できるように変更
    ("member_id")にしているから変更があるなら修正よろしく

--- WEB-INF/classes/command/ShowMemberOrderCommand.java
+++ WEB-INF/classes/command/ShowMemberOrderCommand.java
@@ -28,7 +28,7 @@ public class ShowMemberOrderCommand extends AbstractCommand {
 		
 		RequestContext requestContext = getRequestContext();
 		
-		String memberId = requestContext.getParameter("member_id")[0];
+		String memberId = (String) requestContext.getSessionAttribute("member_id");
 		/*会員情報をリクエストから取得*/
 		
 		ArrayList cart = (ArrayList)requestContext.getSessionAttribute("cart");
